4314a90351eacb4b7572ed13f74c0ea09a14c5e3
Felix Gliesche <felix.gliesche@gmail.com>
2011-07-10 11:17:21 +0200

    some first files

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : ?>
+
+	<?php while (have_posts()) : the_post(); ?>
+
+		<div class="post" id="post-<?php the_ID(); ?>">
+			<h2><?php the_title(); ?></h2>
+
+			<div class="entry">
+				<?php the_content(); ?>
+			</div>
+		</div>
+
+	<?php endwhile; ?>
+
+
+<?php else : ?>
+
+	<h2 class="center">Not Found</h2>
+	<p class="center">Sorry, but you are looking for something that isn't here.</p>
+
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
