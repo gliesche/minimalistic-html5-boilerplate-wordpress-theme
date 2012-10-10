cea66b9c0b756e573048f3fa8d0f2cc2b36f1dd0
Felix Gliesche <felix@fwd.io>
2012-10-10 12:24:21 +0200

    update to html5 boilerplate 4 / removed some comments

--- footer.php
+++ footer.php
@@ -10,13 +10,10 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
 
-  <!-- scripts concatenated and minified via ant build script-->
   <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
-  <!-- end scripts-->
-
-  <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
-       mathiasbynens.be/notes/async-analytics-snippet -->
+        
+  <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
   <script>
       var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
       (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
--- header.php
+++ header.php
@@ -1,33 +1,20 @@
 <!DOCTYPE html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!-- Consider specifying the language of your content by adding the `lang` attribute to <html> -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9"> <![endif]-->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-
-  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-        More info: h5bp.com/i/378 -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
   <title><?php bloginfo('name'); ?> | <?php the_title(); ?></title>
   <meta name="description" content="">
-
-  <!-- Mobile viewport optimized: h5bp.com/viewport -->
   <meta name="viewport" content="width=device-width">
 
-  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-  <!-- CSS: implied media="all" -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-
-  <!-- More ideas for your <head> here: h5bp.com/docs/#head-Tips -->
-
-  <!-- All JavaScript at the bottom, except this Modernizr build.
-        Modernizr enables HTML5 elements & feature detects for optimal performance.
-        Create your own custom Modernizr build: www.modernizr.com/download/ -->
   <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr.js"></script>
 </head>
 
